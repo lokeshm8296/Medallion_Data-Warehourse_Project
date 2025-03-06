eeeef22e6e9a6af29c102389b27c07b15e6aae43
Lokesh M <lokeshreigns9844@gmail.com>
2025-03-06 12:24:26 +0530

    Update ddl_silver_layer.sql

--- scripts/silver/ddl_silver_layer.sql
+++ scripts/silver/ddl_silver_layer.sql
@@ -1,5 +1,13 @@
---- Silver Layer 
-
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 -- Source CRM Table Creation:
 
 -- Customer Information Table
